cc7d59ce7bbaf79aa7b0d06b7a44ce8671d17724
Mark Hopkins <mark.andrew.hopkins@gmail.com>
2024-12-12 11:56:52 -0500

    Added content.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 CSCI 134 Lab 5 ("To the Carpathians!")
 --------------------------------------
 
-The assignment is described in `lab5.pdf`.
+The assignment is described [here](https://docs.google.com/document/d/1y899SowSHmZttiANRdZP2_pUZ95KpY4MFuy3FhaBeas/edit?usp=sharing)
 
 It contains a starter file for the code you need to write for Lab 5:
 - `lab5.py`: Put your code in this file.
---
+++ lab5.py
@@ -0,0 +1,30 @@
+def is_perfect(n):
+    """ Replace this with your code for Question One ("Perfect"). """
+
+
+def perfect_numbers_up_to(k):
+    """ Replace this with your code for Question Two ("Pobody's Nerfect"). """
+
+
+def down_up(n):
+    """ Replace this with your code for Question Three ("The Back Room"). """
+
+
+def make_even(nums):
+    """ Replace this with your code for Question Four ("The Back Room, The Back Room"). """
+
+
+def longest_increasing(array):
+    """ Replace this with your code for Question Five ("Keypad"). """
+
+
+def character_count(s):
+    """ Replace this with your code for Question Six ("Frequencies"). """
+
+
+def map_char(f, msg):
+    """ Replace this with your code for Question Seven ("Maps"). """
+
+
+def ignition(monitor_msg, bunsen_msg):
+    """ Replace this with your code for Question Eight ("Ignition"). """
